152f63f81b9af5b10aed2858ae8f8fb2bd5de9cf
Jon Carbonell <jonatancarmar@gmail.com>
2024-10-16 20:42:38 +0200

    Study the fact of when initializing views and controllers. You can detach the initGui from the constructor if you dont want the views to flash.

--- src/main/java/org/jonatancarbonellmartinez/app/AppInitializer.java
+++ src/main/java/org/jonatancarbonellmartinez/app/AppInitializer.java
@@ -24,6 +24,16 @@ public class AppInitializer {
 
             // Creamos la vista
             PersonView personView = new PersonView();
+            personView.setVisible(false); // la oculto para solo mostrarla cuando la necesite. creo que esto no es buena practica, investigar si es mejor que cree las vistas y controladores a demanda.
+            /**
+             * Híbrido: Una práctica común es combinar ambos enfoques. Puedes inicializar los componentes
+             * que son críticos para la interfaz de usuario al inicio y cargar los demás a demanda. Por ejemplo,
+             * si tienes una pantalla principal que se usa constantemente, la puedes cargar al inicio y otros paneles menos usados se pueden cargar a demanda.
+             *
+             * No hay una respuesta definitiva, ya que la mejor práctica puede variar según las necesidades específicas de tu aplicación y su contexto.
+             * Es recomendable considerar un enfoque que combine ambos métodos, aprovechando las ventajas de cada uno y minimizando sus desventajas.
+             * Además, realizar pruebas de rendimiento y usabilidad puede ayudar a determinar el enfoque más adecuado para tu caso específico.
+             */
 
             // Creamos el controlador y le pasamos el DAO y la vista
             PersonController personController = new PersonController(personDAO, personView);
