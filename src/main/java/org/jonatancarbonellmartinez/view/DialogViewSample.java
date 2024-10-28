9c0406dd795f1dce76bc087a839bcdbceb74358b
jon_laptop <jon.c.m@icloud.com>
2024-10-28 14:36:24 +0100

    Added todo DialogViewSample

---
+++ src/main/java/org/jonatancarbonellmartinez/view/DialogViewSample.java
@@ -0,0 +1,62 @@
+package org.jonatancarbonellmartinez.view;
+
+import javax.swing.*;
+
+public class DialogViewSample extends JDialog implements View, DialogView { // TODO to reuse dialog code. Favor composition over inheritance
+
+
+    @Override
+    public void setupUIProperties() {
+
+    }
+
+    @Override
+    public void createPanels() {
+
+    }
+
+    @Override
+    public void configurePanels() {
+
+    }
+
+    @Override
+    public void assemblePanels() {
+
+    }
+
+    @Override
+    public void createComponents() {
+
+    }
+
+    @Override
+    public void configureComponents() {
+
+    }
+
+    @Override
+    public void assembleComponents() {
+
+    }
+
+    @Override
+    public void addActionListeners() {
+
+    }
+
+    @Override
+    public void createEditModeComponents() {
+
+    }
+
+    @Override
+    public void configureEditModeComponents() {
+
+    }
+
+    @Override
+    public void clearFields() {
+
+    }
+}
