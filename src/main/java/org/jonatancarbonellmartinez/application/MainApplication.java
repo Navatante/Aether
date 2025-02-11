c5a1074ded7c2310dbd6b27569166dd0f49c65b9
jon_laptop <jon.c.m@icloud.com>
2025-02-11 10:28:55 +0100

    DatabaseCheck working good.

--- src/main/java/org/jonatancarbonellmartinez/application/MainApplication.java
+++ src/main/java/org/jonatancarbonellmartinez/application/MainApplication.java
@@ -5,10 +5,14 @@ import javafx.stage.Stage;
 import org.jonatancarbonellmartinez.application.coordinator.MainCoordinator;
 import org.jonatancarbonellmartinez.application.di.AppComponent;
 import org.jonatancarbonellmartinez.application.di.DaggerAppComponent;
+import org.jonatancarbonellmartinez.presentation.viewmodel.DatabaseCheckViewModel;
 
 import javax.inject.Inject;
 
 public class MainApplication extends Application {
+    @Inject
+    DatabaseCheckViewModel databaseCheckViewModel;
+
     @Inject
     MainCoordinator mainCoordinator;
 
@@ -23,6 +27,9 @@ public class MainApplication extends Application {
         // Initialize Dagger
         appComponent = DaggerAppComponent.factory().create();
         appComponent.inject(this);
+
+        // Primero comprobamos conexion a la base de datos, si la conexion no es exitosa y no se elige Db, no se inicia la aplicacion.
+        databaseCheckViewModel.checkAndInitializeDatabase();
 
         // Start coordinator
         mainCoordinator.start(primaryStage);
